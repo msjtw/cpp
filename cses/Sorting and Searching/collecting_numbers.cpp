60628ebe34092327b2dcc85f8e2606edb15a9193
msjtw <fiedler.stanislaw@gmail.com>
2024-02-17 21:07:33 +0100

    cses

---
+++ cses/Sorting and Searching/collecting_numbers.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+const int SIZE = 1e6+7;
+
+int arr[SIZE];
+int res = 1;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n --> 0){
+        int a;
+        cin >> a;
+        if(arr[a-1] > 0){
+            arr[a] = arr[a-1];
+        }
+        else{
+            arr[a] = res;
+            res++;
+        }
+    }
+
+    cout << res-1;
+
+    return 0;
+}
