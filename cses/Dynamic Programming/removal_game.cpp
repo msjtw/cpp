a0fc404f198043131d0a672922ae9e9a9a481c3e
msjtw <fiedler.stanislaw@gmail.com>
2024-02-23 21:28:06 +0100

    cses

--- cses/Dynamic Programming/removal_game.cpp
+++ cses/Dynamic Programming/removal_game.cpp
@@ -3,29 +3,73 @@
 
 using namespace std;
 
-vector<int> arr;
+template <typename T,typename U>                                                   
+std::pair<T,U> operator+(const std::pair<T,U> & l,const std::pair<T,U> & r) {   
+    return {l.first+r.first,l.second+r.second};                                    
+}   
 
-int memo[5007][5007];
+typedef long long int ll;
+const ll SIZE = 1e15+7;
 
-int dp(int p, int q){
-    if(p > q)
-        return 0;
-    if(memo[p][q] > 0){
-        return memo[p][q];  
+pair<ll,ll> player1(ll, ll);
+pair<ll,ll> player2(ll, ll);
+
+vector<ll> arr;
+
+pair<ll,ll> memo1[5007][5007];
+pair<ll,ll> memo2[5007][5007];
+bool set1[5007][5007];
+bool set2[5007][5007];
+
+pair<ll,ll> player1(ll p, ll q){
+    if(set1[p][q])
+        return memo1[p][q];
+
+    pair<ll,ll> ret;
+    if(p == q)
+        ret = {arr[p], 0};
+    else{
+        auto score1 = player2(p+1,q) + make_pair(arr[p],0LL);
+        auto score2 = player2(p,q-1) + make_pair(arr[q],0LL);
+        if(score1.first > score2.first)
+            ret = score1;
+        else
+            ret = score2;
     }
-    int ret = 0;
-    ret  = max(ret, dp());
-    memo[p][q] = ret;
+    set1[p][q] = true;
+    memo1[p][q] = ret;
+    return ret;
+}
+
+pair<ll,ll> player2(ll p, ll q){
+    if(set2[p][q])
+        return memo2[p][q];
+
+    pair<ll,ll> ret;
+    if(p == q)
+        ret = {0, arr[p]};
+    else{
+        auto score1 = player1(p+1,q) + make_pair(0LL, arr[p]);
+        auto score2 = player1(p,q-1) + make_pair(0LL, arr[q]);
+        if(score1.second > score2.second)
+            ret = score1;
+        else
+            ret = score2;
+    }\
+    set2[p][q] = true;
+    memo2[p][q] = ret;
     return ret;
 }
 
 int main(){
-    int n;
+    ll n;
     cin >> n;
-    for(int i = 0; i < n; i++){
-        int a;
+    for(ll i = 0; i < n; i++){
+        ll a;
         cin >> a;
         arr.push_back(a);
     }
+
+    cout << player1(0,n-1).first;
     return 0;
 }
--- cses/Graph Algorithms/round_trip.cpp
+++ cses/Graph Algorithms/round_trip.cpp
@@ -5,9 +5,27 @@ using namespace std;
 
 const int SIZE = 1e5+7;
 
+vector<int> adj[SIZE];
+int vis[SIZE];
+
+int start;
+
+int dfs(int a, int len){
+    vis[a] = true;
+    if(a == start){
+        return 0;
+    }
+}
+
 int main(){
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
 
     return 0;
 }
