6b86a4411e0eeb240ade04a01ad52ff31d6ed6df
msjtw <fiedler.stanislaw@gmail.com>
2024-02-26 22:32:15 +0100

    monsters cses

--- cses/Graph Algorithms/monsters.cpp
+++ cses/Graph Algorithms/monsters.cpp
@@ -1,91 +1,141 @@
-#include<iostream>
-#include<set>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-char map[1007][1007];
-bool monst_map[1007][1007];
-bool vis[1007][1007];
+const int SIZE = 1007;
+typedef pair<int,int> pii;
 
-pair<int,int> start;
-vector <pair<int, int>> monsters;
+int n, m;
 
+pair<int,int> A;
+queue<pair<int,int>> monsters;
+
+char map[SIZE][SIZE];
+
+int dist_A[SIZE][SIZE];
+int dist_mon[SIZE][SIZE];
+
+void bfs_monst(){
+    while(!monsters.empty()){
+        pii p = monsters.front();
+        monsters.pop();
+        pii a = {p.first-1, p.second};
+        if(map[a.first][a.second] == '.' and dist_mon[a.first][a.second] == __INT_MAX__){
+            dist_mon[a.first][a.second] = dist_mon[p.first][p.second] +1;
+            monsters.push(a);
+        }
+        a = {p.first+1, p.second};
+        if(map[a.first][a.second] == '.' and dist_mon[a.first][a.second] == __INT_MAX__){
+            dist_mon[a.first][a.second] = dist_mon[p.first][p.second] +1;
+            monsters.push(a);
+        }
+        a = {p.first, p.second-1};
+        if(map[a.first][a.second] == '.' and dist_mon[a.first][a.second] == __INT_MAX__){
+            dist_mon[a.first][a.second] = dist_mon[p.first][p.second] +1;
+            monsters.push(a);
+        }
+        a = {p.first, p.second+1};
+        if(map[a.first][a.second] == '.' and dist_mon[a.first][a.second] == __INT_MAX__){
+            dist_mon[a.first][a.second] = dist_mon[p.first][p.second] +1;
+            monsters.push(a);
+        }
+    }
+}
+
+pii bfs_A(){
+    queue<pii> q;
+    q.push(A);
+    while(!q.empty()){
+        pii p = q.front();
+        q.pop();
+        if(dist_A[p.first][p.second] >= dist_mon[p.first][p.second]){
+            continue;
+        }
+        if(p.first == 0 or p.second == 0 or p.first == n-1 or p.second == m-1){
+            return p;
+        }
+        pii a = {p.first-1, p.second};
+        if(map[a.first][a.second] == '.' and dist_A[a.first][a.second] == __INT_MAX__){
+            dist_A[a.first][a.second] = dist_A[p.first][p.second] +1;
+            map[a.first][a.second] = 'U';
+            q.push(a);
+        }
+        a = {p.first+1, p.second};
+        if(map[a.first][a.second] == '.' and dist_A[a.first][a.second] == __INT_MAX__){
+            dist_A[a.first][a.second] = dist_A[p.first][p.second] +1;
+            map[a.first][a.second] = 'D';
+            q.push(a);
+        }
+        a = {p.first, p.second-1};
+        if(map[a.first][a.second] == '.' and dist_A[a.first][a.second] == __INT_MAX__){
+            dist_A[a.first][a.second] = dist_A[p.first][p.second] +1;
+            map[a.first][a.second] = 'L';
+            q.push(a);
+        }
+        a = {p.first, p.second+1};
+        if(map[a.first][a.second] == '.' and dist_A[a.first][a.second] == __INT_MAX__){
+            dist_A[a.first][a.second] = dist_A[p.first][p.second] +1;
+            map[a.first][a.second] = 'R';
+            q.push(a);
+        }
+    }
+
+    return {-1,-1};
+}
+
+string path(pii start){
+    string res {};
+    while(start != A){
+        res += map[start.first][start.second];
+        if(map[start.first][start.second] == 'U'){
+            start.first += 1;
+        }
+        else if(map[start.first][start.second] == 'D'){
+            start.first -= 1;
+        }
+        else if(map[start.first][start.second] == 'L'){
+            start.second += 1;
+        }
+        else if(map[start.first][start.second] == 'R'){
+            start.second -= 1;
+        }
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
 
 int main(){
-    int n, m;
     cin >> n >> m;
-
-    for(int i = 1; i <= n; i++){
-        for(int k = 1; k <= m; k++){
+    for(int i = 0; i < n ; i++){
+        for(int k = 0; k < m; k++){
+            dist_A[i][k] = __INT_MAX__;
+            dist_mon[i][k] = __INT_MAX__;
             cin >> map[i][k];
-            if(map[i][k] == 'M'){
-                monsters.push_back({i,k});
-                map[i][k] = '.';
-            }
             if(map[i][k] == 'A'){
-                start = {i,k};
+                A = {i,k};
+                dist_A[i][k] = 0;
+                map[i][k] = '.';
+            }
+            if(map[i][k] == 'M'){
+                monsters.push({i,k});
+                dist_mon[i][k] = 0;
                 map[i][k] = '.';
             }
         }
     }
 
-    queue<pair<int,int>> q;
-    q.push(start);
-    while(!q.empty()){
-        vector<pair<int,int>> mon = monsters;
-        monsters.clear();
-        for(auto a : mon){
-            monst_map[a.first][a.second] = false;
-            if(a.first > 1 and map[a.first-1][a.second] == '.' and !monst_map[a.first-1][a.second]){
-                monsters.push_back({a.first-1, a.second});
-                monst_map[a.first-1][a.second] = true;
-            }
-            if(a.first < n and map[a.first+1][a.second] == '.' and !monst_map[a.first+1][a.second]){
-                monsters.push_back({a.first+1, a.second});
-                monst_map[a.first+1][a.second] = true;
-            }
-            if(a.second > 1 and map[a.first][a.second-1] == '.' and !monst_map[a.first][a.second-1]){
-                monsters.push_back({a.first, a.second-1});
-                monst_map[a.first][a.second-1] = true;
-            }
-            if(a.second < m and map[a.first][a.second+1] == '.' and !monst_map[a.first][a.second+1]){
-                monsters.push_back({a.first, a.second+1});
-                monst_map[a.first][a.second+1] = true;
-            }
-        }
-        int num = q.size();
-        while(num--){
-            auto p = q.front();
-            q.pop();
-            if(monst_map[p.first][p.second]){
-                continue;
-            }
-            if(p.first == 1 or p.second == 1 or p.first == m or p.second == m){
-                cout << "YES";
-                return 0;
-            }
-            if(p.first > 0 and map[p.first-1][p.second] == '.' and !vis[p.first-1][p.second] and !monst_map[p.first-1][p.second]){
-                q.push({p.first-1, p.second});
-                vis[p.first-1][p.second] = true;
-            }
-            if(p.second <= n and map[p.first+1][p.second] == '.' and !vis[p.first+1][p.second] and !monst_map[p.first+1][p.second]){
-                q.push({p.first+1, p.second});
-                vis[p.first+1][p.second] = true;
-            }
-            if(p.second > 0 and map[p.first][p.second-1] == '.' and !vis[p.first][p.second-1] and !monst_map[p.first][p.second-1]){
-                q.push({p.first, p.second-1});
-                vis[p.first][p.second-1] = true;
-            }
-            if(p.second <= m and map[p.first][p.second+1] == '.' and !vis[p.first][p.second+1] and !monst_map[p.first][p.second+1]){
-                q.push({p.first, p.second+1});
-                vis[p.first][p.second+1] = true;
-            }
-        }
+    bfs_monst();
+    pii pos = bfs_A();
+    if(pos == make_pair(-1,-1)){
+        cout << "NO";
     }
-
-    cout << "NO";
+    else{
+        cout << "YES\n";
+        string res = path(pos);
+        cout << res.length() << "\n" << res;
+    }
 
     return 0;
 }
