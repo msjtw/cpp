f225a04e0d2562ce450962d2ba55ca5c1b3bf22a
msjtw <fiedler.stanislaw@gmail.com>
2024-04-15 13:11:47 +0200

    cses flows

---
+++ cses/Mathematics/binomial_coefficient.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int arr
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+    }
+    return 0;
+}
